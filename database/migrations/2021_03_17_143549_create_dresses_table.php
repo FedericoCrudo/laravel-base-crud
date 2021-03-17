cbf90dc0f18b5b56ec6156dda4e10e7433ccebc9
Federico <crudofederico@gmail.com>
2021-03-17 17:11:36 +0100

    work in progress

---
+++ database/migrations/2021_03_17_143549_create_dresses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('descrizione');
+            $table->float('price',7,2);
+            $table->text('img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dress');
+    }
+}
