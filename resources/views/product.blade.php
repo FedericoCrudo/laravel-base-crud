1b48ccc240a91ae016fa4b0b09a15a3e14eb519f
Federico <crudofederico@gmail.com>
2021-03-18 16:48:31 +0100

    fix responsive

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -11,17 +11,20 @@ Product
         
     <div class="card-deck mt-3 mb-3">
         @foreach($vestiti as $items)
-        <div class="card">
-            <img class="card-img-top" src="{{$items->img}}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">{{$items->nome}}</h5>
-                <p class="card-text">{{$items->descrizione}}</p> 
+        <div class="col-lg-4">
+              <div class="card">
+                <img class="card-img-top" src="{{$items->img}}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{$items->nome}}</h5>
+                    <p class="card-text">{{$items->descrizione}}</p> 
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">{{$items->price}} €</small>
+                </div>
+                <a class="text-center"href=" {{route('dress.show',['dress'=>$items->id])}}">Info</a>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">{{$items->price}} €</small>
-            </div>
-            <a class="text-center"href=" {{route('dress.show',['dress'=>$items->id])}}">Info</a>
         </div>
+      
         @endforeach
     </div>  
 @endsection
